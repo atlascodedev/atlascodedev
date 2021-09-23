d3d60bd41ca93d51d0c45f2dbded6d8ed2fcd554
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-23 11:50:44 -0300

    [feature basic mobile menu layout and animation

--- libs/frontend-components/src/lib/MobileMenu/MobileMenu.stories.tsx
+++ libs/frontend-components/src/lib/MobileMenu/MobileMenu.stories.tsx
@@ -1,9 +1,25 @@
+import { AppLayout } from '../AppLayout/AppLayout';
 import { Story, Meta } from '@storybook/react';
 import { MobileMenu, MobileMenuProps } from './MobileMenu';
+import { Box } from '@mui/material';
 
 export default {
   component: MobileMenu,
   title: 'MobileMenu',
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'brazilDesktop1',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <AppLayout>
+        <Box sx={{ height: '55vh' }} />
+        <Story />
+      </AppLayout>
+    ),
+  ],
 } as Meta;
 
 const Template: Story<MobileMenuProps> = (args) => <MobileMenu {...args} />;
